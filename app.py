98e0f145525cd2dd60c492d138340d7608a67b8e
Chr0m0s0m3s <mitochondria69@protonmail.com>
2020-10-06 23:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
-class Helloworld(Resource):
+class landingpage(Resource):
     def post(self):
         some_json = request.get_json()
 
-class Multi(Resource):
+class search(Resource):
     def get(self, num):
         fbook = fb.is_valid(num)
         time.sleep(5)
@@ -38,5 +38,7 @@ class Multi(Resource):
             }
 
 
-api.add_resource(Helloworld, "/")
-api.add_resource(Multi, '/<num>')
+api.add_resource(landingpage, "/")
+api.add_resource(search, '/<num>')
+if __name__ == "__main__":
+    app.run()
